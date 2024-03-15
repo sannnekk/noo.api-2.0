4706d7c02b455df804ffbbb2466215803e34f12c
Mona Lisa <nukleoid@outlook.com>
2024-03-15 21:55:17 +0100

    [FIX] Payload size set to 15 mb for urlencoded

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@ await CoreDataSource.initialize();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 app.use(MediaMiddleware);
 setContextClass(Context);
 injectControllers(app);
--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true, limit: '15mb' }))
 
 app.use(MediaMiddleware)
 
