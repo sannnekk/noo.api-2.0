943237dee2dee919b35d3571484ac2617bc417b0
sannnekk <nukleoid@outlook.com>
2024-06-03 11:18:30 +0200

    [FIX] Stable version

--- dist/Core/Errors/RoleChangedButNotReloggedInError.js
+++
@@ -1,9 +0,0 @@
-import { StatusCodes } from 'http-status-codes';
-export class RoleChangedButNotReloggedInError extends Error {
-    code = StatusCodes.CONFLICT;
-    message;
-    constructor(message = 'Похоже, у этого аккаунта изменилась роль. Пожалуйста, перезайдите.') {
-        super();
-        this.message = message;
-    }
-}
