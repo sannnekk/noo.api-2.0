39f1c7a1fbc68c80ee85cf7a2dc0d80f8f835972
sannnekk <nukleoid@outlook.com>
2024-06-03 10:38:53 +0200

    [BUILD] 2

--- dist/Core/Request/ContextMiddleware.js
+++
@@ -1,6 +0,0 @@
-import { Context } from './Context.js';
-export function ContextMiddleware(req, res, next) {
-    // @ts-ignore
-    req.context = new Context(req);
-    next();
-}
--- dist/Media/Data/MediaRepository.js
+++
@@ -1,7 +0,0 @@
-import { Repository } from '../../Core/Data/Repository.js';
-import { MediaModel } from './MediaModel.js';
-export class MediaRepository extends Repository {
-    constructor() {
-        super(MediaModel);
-    }
-}
