e4647a814e97f11a349bf3b2641f7bcb9b38cc60
sannnekk <nukleoid@outlook.com>
2024-12-16 04:14:20 +0100

    [IMPR] Hardest tasks are now selected relative to the highest score

--- dist/Works/Data/WorkTaskRepository.js
+++ dist/Works/Data/WorkTaskRepository.js
@@ -7,7 +7,7 @@ export class WorkTaskRepository extends Repository {
     async getHardestTaskIds(workId, count) {
         return this.queryBuilder('work_task')
             .select('work_task.id', 'taskId')
-            .addSelect('COALESCE(AVG(awc.score), 0)', 'avgScore')
+            .addSelect('COALESCE(AVG(awc.score / work_task.highest_score * 100), 0)', 'avgScore')
             .leftJoin('assigned_work_comment', 'awc', 'work_task.id = awc.taskId')
             .where('work_task.workId = :workId', { workId })
             .andWhere('awc.score IS NOT NULL')
--- src/Works/Data/WorkTaskRepository.ts
+++ src/Works/Data/WorkTaskRepository.ts
@@ -14,7 +14,10 @@ export class WorkTaskRepository extends Repository<WorkTask> {
   ): Promise<{ taskId: string; avgScore: number }[]> {
     return this.queryBuilder('work_task')
       .select('work_task.id', 'taskId')
-      .addSelect('COALESCE(AVG(awc.score), 0)', 'avgScore')
+      .addSelect(
+        'COALESCE(AVG(awc.score / work_task.highest_score * 100), 0)',
+        'avgScore'
+      )
       .leftJoin('assigned_work_comment', 'awc', 'work_task.id = awc.taskId')
       .where('work_task.workId = :workId', { workId })
       .andWhere('awc.score IS NOT NULL')
