d13d9204459e522590de73f7e52d68df16ab75e0
sannnekk <nukleoid@outlook.com>
2025-05-12 00:32:50 +0200

    [IMPR] Added db check to health check endpoint

--- dist/Platform/Services/HealthCheckService.js
+++ dist/Platform/Services/HealthCheckService.js
@@ -1,10 +1,29 @@
+import { UserRepository } from '../../Users/Data/UserRepository.js';
 export class HealthCheckService {
+    userRepository;
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
     async healthcheck() {
-        return [
+        const results = [
             {
                 label: 'Event loop',
                 status: 'ok',
             },
         ];
+        try {
+            const userCount = await this.userRepository.count();
+            results.push({
+                label: 'Users',
+                status: userCount > 0 ? 'ok' : 'warning',
+            });
+        }
+        catch (error) {
+            results.push({
+                label: 'Database: ' + error.message,
+                status: 'error',
+            });
+        }
+        return results;
     }
 }
--- src/Platform/Services/HealthCheckService.ts
+++ src/Platform/Services/HealthCheckService.ts
@@ -1,15 +1,39 @@
+import { UserRepository } from '@modules/Users/Data/UserRepository'
+
 export type HealthCheckResult = {
   label: string
   status: 'ok' | 'error' | 'warning'
 }
 
 export class HealthCheckService {
+  private readonly userRepository: UserRepository
+
+  public constructor() {
+    this.userRepository = new UserRepository()
+  }
+
   public async healthcheck(): Promise<HealthCheckResult[]> {
-    return [
+    const results: HealthCheckResult[] = [
       {
         label: 'Event loop',
         status: 'ok',
       },
     ]
+
+    try {
+      const userCount = await this.userRepository.count()
+
+      results.push({
+        label: 'Users',
+        status: userCount > 0 ? 'ok' : 'warning',
+      })
+    } catch (error: any) {
+      results.push({
+        label: 'Database: ' + error.message,
+        status: 'error',
+      })
+    }
+
+    return results
   }
 }
